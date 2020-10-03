fa6b7cee4434fec1274c18bc5b2726487721aa92
rutthaphol <53845140+rutthaphol@users.noreply.github.com>
2020-10-03 22:46:13 +0700

    first commit
    first inititial

---
+++ index2.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+var path = require('path');
+const { deflate } = require('zlib');
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.engine('html' , require('ejs').renderFile)
+app.set('view engine' , 'html')
+
+//app.use(morgan('tiny'));
+app.use(cors());
+app.use(bodyParser.json());
+
+app.use(express.static(path.join(__dirname, '/')));
+ 
+const port = process.env.PORT || 4000;
+app.listen(port, () => {
+    console.log(`listening on ${port}`);
+});
+
+app.get('/', (req, res) => {
+    res.sendFile( path.join(__dirname + '/index.html') );
+})
+app.post('/rooms', (req, res) => {
+    res.render( path.join(__dirname + '/rooms.html') , { name : new Date(req.body.start_date)});
+
+    console.log(req.body);
+})
+
---
+++ js/main.js
@@ -0,0 +1,150 @@
+/*  ---------------------------------------------------
+    Template Name: Sona
+    Description: Sona Hotel Html Template
+    Author: Colorlib
+    Author URI: https://colorlib.com
+    Version: 1.0
+    Created: Colorlib
+---------------------------------------------------------  */
+
+'use strict';
+
+(function ($) {
+
+    /*------------------
+        Preloader
+    --------------------*/
+    $(window).on('load', function () {
+        $(".loader").fadeOut();
+        $("#preloder").delay(200).fadeOut("slow");
+    });
+
+    /*------------------
+        Background Set
+    --------------------*/
+    $('.set-bg').each(function () {
+        var bg = $(this).data('setbg');
+        $(this).css('background-image', 'url(' + bg + ')');
+    });
+
+    //Offcanvas Menu
+    $(".canvas-open").on('click', function () {
+        $(".offcanvas-menu-wrapper").addClass("show-offcanvas-menu-wrapper");
+        $(".offcanvas-menu-overlay").addClass("active");
+    });
+
+    $(".canvas-close, .offcanvas-menu-overlay").on('click', function () {
+        $(".offcanvas-menu-wrapper").removeClass("show-offcanvas-menu-wrapper");
+        $(".offcanvas-menu-overlay").removeClass("active");
+    });
+
+    // Search model
+    $('.search-switch').on('click', function () {
+        $('.search-model').fadeIn(400);
+    });
+
+    $('.search-close-switch').on('click', function () {
+        $('.search-model').fadeOut(400, function () {
+            $('#search-input').val('');
+        });
+    });
+
+    /*------------------
+		Navigation
+	--------------------*/
+    $(".mobile-menu").slicknav({
+        prependTo: '#mobile-menu-wrap',
+        allowParentLinks: true
+    });
+
+    /*------------------
+        Hero Slider
+    --------------------*/
+   $(".hero-slider").owlCarousel({
+        loop: true,
+        margin: 0,
+        items: 1,
+        dots: true,
+        animateOut: 'fadeOut',
+        animateIn: 'fadeIn',
+        smartSpeed: 1200,
+        autoHeight: false,
+        autoplay: true,
+        mouseDrag: false
+    });
+
+    /*------------------------
+		Testimonial Slider
+    ----------------------- */
+    $(".testimonial-slider").owlCarousel({
+        items: 1,
+        dots: false,
+        autoplay: true,
+        loop: true,
+        smartSpeed: 1200,
+        nav: true,
+        navText: ["<i class='arrow_left'></i>", "<i class='arrow_right'></i>"]
+    });
+
+    /*------------------
+        Magnific Popup
+    --------------------*/
+    $('.video-popup').magnificPopup({
+        type: 'iframe'
+    });
+
+    /*------------------
+		Date Picker
+	--------------------*/
+    // $(".date-input").datepicker({
+    //     minDate: 0,
+    //     dateFormat: 'dd MM, yy',
+    // });
+    // $(".date-output").datepicker(
+    //     {
+    //     minDate: $(".date-input").value,
+    //     dateFormat: 'dd MM, yy',
+    //     onSelect: function(dateText) {
+    //         console.log(  $(".date-input").val() );
+    //     }
+    // });
+    var dateToday = new Date(Date.now()+ 1 * 24*60*60*1000); 
+    var dateTomorrow = new Date(dateToday.getTime() + 1 * 24*60*60*1000); 
+    $( function() {
+    $( "#date-in" ).datepicker({
+        changeMonth: true,
+        changeYear: true,
+        dateFormat:'dd MM, yy',
+        minDate: dateToday,
+        onClose: function (selected) {
+        if(selected.length <= 0) {
+            // selected is empty
+            $("#date-out").datepicker('disable');
+        } else {
+            $("#date-out").datepicker('enable');
+        }
+        $("#date-out").datepicker("option", "minDate", new Date(new Date(selected).getTime() + 1 * 24*60*60*1000));
+        }
+    });
+    $( "#date-out" ).datepicker({
+        changeMonth: true,
+        changeYear: true,
+        dateFormat:'dd MM, yy',
+        minDate: dateTomorrow,
+        onClose: function (selected) {
+        if(selected.length <= 0) {
+            // selected is empty
+            $("#date-in").datepicker('disable');
+        } else {
+            $("#date-in").datepicker('enable');
+        }
+        // $("#date-in").datepicker("option", "maxDate", selected);
+        }
+    });
+    }); 
+    /*------------------
+		Nice Select
+	--------------------*/
+    $("select").niceSelect();
+
+})(jQuery);
