c8ded6f6ed7902cfc5cadf07a3a1ee876d9114ea
rutthaphol <rutthaphol.s@ku.th>
2020-10-19 14:20:50 +0700

    Revised version
    ระบบเช่าห้อง

--- index2.js
+++ index2.js
@@ -47,7 +47,7 @@ app.get('/rooms', (req, res) => {
         con.query("SELECT DISTINCT Name,Adults,Children,Price,Picture FROM room_desc", function (err, result, fields) {
             if (err) throw err;
             res.render( path.join(__dirname + '/rooms.html') , {CheckPeople : result});
-            console.log(result);
+            // console.log(result);
             con.end()
       });
     });
@@ -57,7 +57,6 @@ app.post('/rooms', (req, res) => {
     var con = mysql.createConnection(config);
     let FormatStartDate = new Date(req.body['start_date']);
     FormatStartDate.setDate(FormatStartDate.getDate() + 1);
-    console.log(FormatStartDate);
     let FormatEndDate = new Date(req.body['end_date']);
     FormatEndDate.setDate(FormatEndDate.getDate() + 1);
     FormatStartDate = FormatStartDate.toISOString().substring(0, 10);
@@ -67,7 +66,7 @@ app.post('/rooms', (req, res) => {
     "(B.Start_Date >= " + "'" + FormatStartDate + "'" + 
     " AND B.Start_Date <" + "'" + FormatEndDate + "'" + ") OR (B.End_Date >= "
      + "'" + FormatStartDate + "'" + " AND B.End_Date <" + "'" + FormatEndDate + "'" + "))";
-    console.log(Query);
+    // console.log(Query);
     con.connect(function(err) {
     if (err) throw err;
         con.query(Query, function (err, result, fields) {
@@ -76,36 +75,45 @@ app.post('/rooms', (req, res) => {
             { name : new Date(req.body.start_date), endDate : new Date(req.body.end_date),
                 Adult : req.body.guest_name, children : req.body.childrens, 
                 CheckPeople : result});
-            console.log(result);
+            // console.log(result);
             con.end()
       });
     });
-    // res.render( path.join(__dirname + '/rooms.html') , { name : new Date(req.body.start_date)
-    //     , endDate : new Date(req.body.end_date), Adult : req.body.guest_name, 
-    //     children : req.body.room_name});
-    // console.log(req.body);
 })
 //api booking rooms
 app.get('/rooms/booking', (req, res) => {
-    res.render( path.join(__dirname + '/booking-rooms.html') , {startDate : req.query.custStartDate, 
-        endDate : req.query.custEndDate, Adult : req.query.custAdults, children : req.query.custChildren, 
+    let FormatStartDate = new Date(req.query['custStartDate']);
+    FormatStartDate.setDate(FormatStartDate.getDate() + 1);
+    let FormatEndDate = new Date(req.query['custEndDate']);
+    FormatEndDate.setDate(FormatEndDate.getDate() + 1);
+    FormatStartDate = FormatStartDate.toISOString().substring(0, 10);
+    FormatEndDate = FormatEndDate.toISOString().substring(0, 10);
+    res.render( path.join(__dirname + '/booking-rooms.html') , {startDate : FormatStartDate, 
+        endDate : FormatEndDate, Adult : req.query.custAdults, children : req.query.custChildren, 
         room_id : req.query.custId, Style : req.query.custName, Price : req.query.custPrice });
 })
 
 app.post('/rooms/booking/Payment_receipt', (req, res) => {
     let FormatStartDate = new Date(req.body['custStartDate']);
-    FormatStartDate.setDate(FormatStartDate.getDate() + 1);
+    FormatStartDate.setDate(FormatStartDate.getDate());
     let FormatEndDate = new Date(req.body['custEndDate']);
-    FormatEndDate.setDate(FormatEndDate.getDate() + 1);
+    FormatEndDate.setDate(FormatEndDate.getDate());
     FormatStartDate = FormatStartDate.toISOString().substring(0, 10);
     FormatEndDate = FormatEndDate.toISOString().substring(0, 10);
-
+    console.log(FormatEndDate);
     let QueryCheck = "SELECT ID from room_booking where " +
     "((Start_Date >= " + "'" + FormatStartDate + "'" + 
     " AND Start_Date <" + "'" + FormatEndDate + "'" + ") OR (End_Date >= "
      + "'" + FormatStartDate + "'" + " AND End_Date <" + "'" + FormatEndDate + "'" + ")) AND ID = '" + req.body.custId + "';";
+
+    let QueryCheckAvailable = "SELECT ID FROM room_desc AS D where D.ID NOT IN (SELECT B.ID from room_booking AS B  WHERE (B.Start_Date >=" +
+    "'" + FormatStartDate + "'" + 
+     " AND B.Start_Date < " + "'" + FormatEndDate + "'" + ") OR (B.End_Date >= "+ "'" + 
+     FormatStartDate + "'" + " AND B.End_Date <" + "'" + FormatEndDate + "'" + ")) AND D.Name = '" + req.body.custName + "';";
+    
     let QueryInsertBooking = "INSERT INTO room_booking (ID,Start_Date,End_Date) VALUES (" + "'" + req.body.custId + "'" +
      ", '" + FormatStartDate + "' , '" + FormatEndDate + "');";
+    
     let QueryLog = "INSERT INTO rental_record (Room_number,Room_name,CheckIn,CheckOut,Name,Email,Address,Phone_number,State,Zip) VALUES ('" + 
     req.body.custId + "', '" + req.body.custName + "', '" + FormatStartDate + "', '" + FormatEndDate + "', '" + req.body.firstname + "', '"
     + req.body.email + "', '" + req.body.address + "', '" + req.body.Phone + "', '" + req.body.state + "', '" + req.body.zip + "');";
@@ -113,23 +121,53 @@ app.post('/rooms/booking/Payment_receipt', (req, res) => {
     con.connect(function(err) {
         if (err) throw err;
             con.query(QueryCheck, function (err, result, fields) {
-                console.log(result);
+                // console.log(result);
                 if (err) throw err;
-                if (result.length != 0) res.redirect("/?msg=failed")//res.render( path.join(__dirname + '/index.html') , {Message : "Failed"});
+                if (result.length != 0)//ถูกเช่าไปแล้ว ทำการค้นหาห้องใหม่อีกครั้ง
+                {
+                    con.query(QueryCheckAvailable, function (err, result3, fields) {//เช็คห้องห้องว่างห้องใหม่
+                        if (err) throw err;
+                        if(result3.length >= 1)
+                        {
+                            let QueryInsertBooking2 = "INSERT INTO room_booking (ID,Start_Date,End_Date) VALUES (" + "'" + result3[0].ID + "'" +
+                            ", '" + FormatStartDate + "' , '" + FormatEndDate + "');";
+                            con.query(QueryInsertBooking2, function (err, result1, fields) {
+                                if (err) throw err;
+                                let QueryLog2 = "INSERT INTO rental_record (Room_number,Room_name,CheckIn,CheckOut,Name,Email,Address,Phone_number,State,Zip) VALUES ('" + 
+                                result3[0].ID + "', '" + req.body.custName + "', '" + FormatStartDate + "', '" 
+                                + FormatEndDate + "', '" + req.body.firstname + "', '"+ req.body.email + "', '" + 
+                                req.body.address + "', '" + req.body.Phone + "', '" + req.body.state + "', '" + req.body.zip + "');";
+                                con.query(QueryLog2, function (err, result2, fields) {
+                                    if (err) throw err;
+                                    res.render( path.join(__dirname + '/signUp.html') , {Name : req.body.firstname, Email : req.body.email,
+                                    Phone : req.body.Phone, StartDate : req.body.custStartDate, endDate : req.body.custEndDate, 
+                                    Adult : req.body.custAdults, children : req.body.custChildren,Style : req.body.custName, 
+                                    Price : req.body.custPrice});
+                                    con.end()
+                                });
+                            });
+                            //console.log(result3[0].ID);
+                        }
+                        else{
+                            res.redirect("/?msg=failed")//res.render( path.join(__dirname + '/index.html') , {Message : "Failed"});
+                            con.end()
+                        }
+                    });
+                } 
                 else{
                     con.query(QueryInsertBooking, function (err, result1, fields) {
                         if (err) throw err;
-                        
                         con.query(QueryLog, function (err, result2, fields) {
                             if (err) throw err;
-                            
-                            res.render( path.join(__dirname + '/signUp.html') , {CheckPeople : result2});
-                            console.log(result);
+                            res.render( path.join(__dirname + '/signUp.html') , {Name : req.body.firstname, Email : req.body.email,
+                                Phone : req.body.Phone, StartDate : req.body.custStartDate, endDate : req.body.custEndDate, 
+                                Adult : req.body.custAdults, children : req.body.custChildren,Style : req.body.custName, 
+                                Price : req.body.custPrice});
+                            // console.log(result);
                             con.end()
                         });
                     });
-                }
-                
+                } 
           });
     });
 })
